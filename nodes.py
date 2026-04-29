27171f94c3a11efdcd82fe46c2f09a1318213af3
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2026-04-28 17:45:44 -0700

    test

--- nodes.py
+++ nodes.py
@@ -243,7 +243,7 @@ def _alarm_nodedefs() -> list[dict]:
                                 {
                                     "id": "SENDCMD",
                                     "name": "Send Command",
-                                    "params": [
+                                    "parameter": [
                                         {
                                             "id": "CMD",
                                             "name": "Command",
